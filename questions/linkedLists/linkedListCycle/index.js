6af6eba11ad6f4ef3cd27a7d06eca78d25565151
Brian <briancmuhammad@yahoo.com>
2024-10-31 14:13:24 -0700

    updated linked cycle list task. Finished reversed linked list task

--- questions/linkedLists/linkedListCycle/index.js
+++ questions/linkedLists/linkedListCycle/index.js
@@ -12,16 +12,16 @@
 
  const listCycle = (head) => {
 
-    let fast = head
-    let slow  = head
-  
-    while (fast && fast.next){
-      slow = slow.next
-      fast = fast.next.next
-  
-      if (slow === fast){
-        return true
-      }
+    const set = new Set()
+    let curr = head
+    
+    while (curr){
+        if (set.has(curr)){
+            return true
+        }
+
+        set.add(curr)
+        curr = curr.next
     }
   
     return false
--- questions/linkedLists/middleOfLinkedList/index.js
+++ questions/linkedLists/middleOfLinkedList/index.js
@@ -11,6 +11,15 @@
 
  const middle = (head) => {
 
+  let slow = head
+  let fast = head
+
+  while (fast && fast.next){
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  return slow
 
 }
 
