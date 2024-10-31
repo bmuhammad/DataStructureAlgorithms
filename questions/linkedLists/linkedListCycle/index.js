d2663d7cdc9d6225dddb90105a1aecf05e017a48
Brian <briancmuhammad@yahoo.com>
2024-10-31 14:05:16 -0700

    linked Cycle List tasks completed

--- questions/linkedLists/linkedListCycle/index.js
+++ questions/linkedLists/linkedListCycle/index.js
@@ -12,7 +12,20 @@
 
  const listCycle = (head) => {
 
-
+    let fast = head
+    let slow  = head
+  
+    while (fast && fast.next){
+      slow = slow.next
+      fast = fast.next.next
+  
+      if (slow === fast){
+        return true
+      }
+    }
+  
+    return false
+  
 }
 
 
