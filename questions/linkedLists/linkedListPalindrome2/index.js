87bb5adc44d5bc4ff247f5cbb5b4c297629a712a
Brian <briancmuhammad@yahoo.com>
2024-10-31 14:02:08 -0700

    Palindrome linked list tasks completed

--- questions/linkedLists/linkedListPalindrome2/index.js
+++ questions/linkedLists/linkedListPalindrome2/index.js
@@ -11,7 +11,37 @@
  */
 
 const isPalindrome = (head) => {
-
+    let slow = head;
+    let fast = head;
+  
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+  
+    let curr = slow;
+    let prev = null;
+  
+    while (curr) {
+      let next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+  
+    let l = head;
+    let r = prev;
+  
+    while (r) {
+      if (l.val !== r.val) {
+        return false;
+      }
+  
+      l = l.next;
+      r = r.next;
+    }
+  
+    return true;
 };
 
 module.exports = isPalindrome;
